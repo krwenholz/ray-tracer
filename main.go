386ec2469701f757621e5b74efe6e2d1711af2f5
Kyle Wenholz <kyle@ngrok.com>
2023-02-20 11:57:14 -0800

    Simple ngrok file server for testing later

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"golang.ngrok.com/ngrok"
+	"golang.ngrok.com/ngrok/config"
+)
 
 func main() {
-	fmt.Println("hello")
+	if err := run(context.Background()); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(ctx context.Context) error {
+	tun, err := ngrok.Listen(ctx,
+		config.HTTPEndpoint(
+			config.WithOAuth("github", config.WithAllowOAuthEmail("kyle@krwenholz.com")),
+			config.WithDomain(os.Getenv("DOMAIN")),
+		),
+		ngrok.WithAuthtokenFromEnv(),
+	)
+	if err != nil {
+		return err
+	}
+
+	log.Println("tunnel created:", tun.URL())
+
+	return http.Serve(tun, http.HandlerFunc(handler))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./main.go")
 }
